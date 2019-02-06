319d49ef257d7809c5b0ccc1c65e8845fe3223f3
Joe DEspinosa <jtdespinosa@gmail.com>
2019-02-06 17:28:13 -0500

    Testing audio js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,11 +3,27 @@ const audioContext = new AudioContext();
 
 const audioElement1 = document.getElementById('audio1');
 const audioElement2 = document.getElementById('audio2');
+
 const track1 = audioContext.createMediaElementSource(audioElement1);
-const track2 = audioContext.createMediaElementSource(audioElement2);s
+const track2 = audioContext.createMediaElementSource(audioElement2);
 
 track1.connect(audioContext.destination);
 track2.connect(audioContext.destination);
 
 const playButton1 = document.getElementById('button1');
 const playButton2 = document.getElementById('button2');
+
+playButton1.addEventListener('click', function () {
+  if (audioContext.state === 'suspended') {
+    audioContext.resume();
+  }
+  if (this.dataset.playing === "false") {
+    audioElement1.play();
+    this.dataset.playing = "true";
+  }
+  if (this.dataset.playing === "true") {
+    audioElement1.pause();
+    this.dataset.playing = "false";
+  }
+}, false);
+
